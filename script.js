a39dcc59dd9bc4bb192a2a6ef903fa6287aa0190
Junaid <Junaid.562000@gmail.com>
2021-02-10 20:21:29 +0530

    JavaScript functionality added

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ window.addEventListener('keydown', (e) => {
     <div class="key">${e.key === ' ' ? 'Space' : e.key}
     <small>event.key</small>
     </div>
-    <div class="key">${e.key === ' ' ? 'Space' : e.key}
+    <div class="key">${e.keyCode}
     <small>event.keyCode</small>
     </div>
-    <div class="key">${e.key === ' ' ? 'Space' : e.key}
+    <div class="key">${e.code}
     <small>event.code</small>
     </div>
     `;
